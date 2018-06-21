25594b1c97fffe0a81a3049eefd6cbd2d4b747ea
luohao3 <luohao3@360.cn>
2018-06-21 14:47:08 +0800

    添加弹出

--- src/renderer/components/LandingPage/SystemInformation.vue
+++ src/renderer/components/LandingPage/SystemInformation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <!-- <div>
     <div class="title">Information</div>
     <div class="items">
       <div class="item">
@@ -27,47 +27,47 @@
         <div class="value">{{ platform }}</div>
       </div>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        electron: process.versions['atom-shell'],
-        name: this.$route.name,
-        node: process.versions.node,
-        path: this.$route.path,
-        platform: require('os').platform(),
-        vue: require('vue/package.json').version
-      }
-    }
-  }
-</script>
+//   export default {
+//     data () {
+//       return {
+//         electron: process.versions['atom-shell'],
+//         name: this.$route.name,
+//         node: process.versions.node,
+//         path: this.$route.path,
+//         platform: require('os').platform(),
+//         vue: require('vue/package.json').version
+//       }
+//     }
+//   }
+// </script>
 
-<style scoped>
-  .title {
-    color: #888;
-    font-size: 18px;
-    font-weight: initial;
-    letter-spacing: .25px;
-    margin-top: 10px;
-  }
+// <style scoped>
+//   .title {
+//     color: #888;
+//     font-size: 18px;
+//     font-weight: initial;
+//     letter-spacing: .25px;
+//     margin-top: 10px;
+//   }
 
-  .items { margin-top: 8px; }
+//   .items { margin-top: 8px; }
 
-  .item {
-    display: flex;
-    margin-bottom: 6px;
-  }
+//   .item {
+//     display: flex;
+//     margin-bottom: 6px;
+//   }
 
-  .item .name {
-    color: #6a6a6a;
-    margin-right: 6px;
-  }
+//   .item .name {
+//     color: #6a6a6a;
+//     margin-right: 6px;
+//   }
 
-  .item .value {
-    color: #35495e;
-    font-weight: bold;
-  }
-</style>
+//   .item .value {
+//     color: #35495e;
+//     font-weight: bold;
+//   }
+// </style>
